ca2223a0d483cfb3a644db8a28057de7ba66b17c
Melody Universe <melody@computost.com>
2025-03-29 11:34:39 -0700

    display selected image instead of placeholder

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,11 +1,15 @@
 import { Image, ImageSource } from "expo-image";
 import { StyleSheet } from "react-native";
 
-export default function ImageViewer({ imgSource }: Props) {
-  return <Image source={imgSource} style={styles.image} />;
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource: ImageSource = selectedImage
+    ? { uri: selectedImage }
+    : imgSource;
+
+  return <Image source={imageSource} style={styles.image} />;
 }
 
-type Props = { imgSource: ImageSource };
+type Props = { imgSource: ImageSource; selectedImage?: string };
 
 const styles = StyleSheet.create({
   image: { width: 320, height: 440, borderRadius: 18 },
