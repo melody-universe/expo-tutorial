3524149c4731da25e02265182a784d435341b974
Melody Universe <melody@computost.com>
2025-03-29 12:15:40 -0700

    make emoji sticker animated

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,11 +1,13 @@
-import { Image, ImageSource } from "expo-image";
+import { ImageSource } from "expo-image";
+import Animated from "react-native-reanimated";
 import { View } from "react-native";
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   return (
     <View style={{ top: -350 }}>
-      <Image
+      <Animated.Image
         source={stickerSource}
+        resizeMode="contain"
         style={{ width: imageSize, height: imageSize }}
       />
     </View>
